94346c93756b164ca3eec6b248f1eee9e61e9041
Jeo D <jeo.d@ibotta.com>
2023-12-01 12:42:31 -0700

    Complete part 1 of day 1

--- day01/solution.js
+++ day01/solution.js
@@ -1,5 +1,15 @@
 import readInput from "../read-input.js";
 
-const input = readInput();
+const main = (input) => {
+  const numbers = input.map(line => {
+    const digitRegex = /\d/;
+    const firstDigit = line.match(digitRegex)[0];
+    const reversedLine = line.split('').reverse().join();
+    const lastDigit = reversedLine.match(digitRegex)[0];
+    return (Number(firstDigit) * 10) + Number(lastDigit);
+  });
+  return numbers.reduce((sum, num) => sum + num, 0);
+};
 
-console.log(input);
+const result = main(readInput());
+console.log(result);
