7e0ddfac6e763da222d5efad1afb42e89c8ac0cb
Jeo D <jeo.d@ibotta.com>
2023-12-01 12:33:55 -0700

    Add JSDoc documentation for readInput function

--- read-input.js
+++ read-input.js
@@ -2,9 +2,13 @@ import { readFileSync } from 'fs';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
-const readInput = () => {
+/**
+ * Returns an array of strings where each element is a line of the text file
+ * @param {string} fileName
+ * @returns {string[]}
+ */
+const readInput = (fileName = process.argv[2]) => {
   const directory = dirname(fileURLToPath(import.meta.url));
-  const fileName = process.argv[2];
   const filePath = join(directory, fileName);
   const rawInput = readFileSync(filePath, {encoding:'utf8', flag:'r'});
   return rawInput.split('\n').map(line => line.replaceAll('\r', ''));
