59d708ec102a3e3670cb0e717b4a87c905110035
Jeo D <jeo.d@ibotta.com>
2023-12-01 14:17:24 -0700

    Complete part 2 of day 1

--- day01/solution.js
+++ day01/solution.js
@@ -1,12 +1,58 @@
 import readInput from "../read-input.js";
 
+const regexes = [
+  /\d/g,
+  /one/g,
+  /two/g,
+  /three/g,
+  /four/g,
+  /five/g,
+  /six/g,
+  /seven/g,
+  /eight/g,
+  /nine/g
+];
+
+const numberWordToNumber = {
+  'one': 1,
+  'two': 2,
+  'three': 3,
+  'four': 4,
+  'five': 5,
+  'six': 6,
+  'seven': 7,
+  'eight': 8,
+  'nine': 9,
+};
+
+const findDigit = (line, sortFunction) => {
+  const matches = regexes.flatMap(regex => [...line.matchAll(regex)]);
+  const sortedMatches = matches.sort(sortFunction);
+  const number = sortedMatches[0][0];
+  if (Number(number)) {
+    return Number(number);
+  } else {
+    return Number(numberWordToNumber[number]);
+  }
+};
+
+const sortByLowestIndexToHighest = (a, z) => {
+  if (a.index < z.index) return -1;
+  if (a.index > z.index) return 1;
+  return 0;
+};
+
+const sortByHighestIndexToLowest = (a, z) => {
+  if (a.index < z.index) return 1;
+  if (a.index > z.index) return -1;
+  return 0;
+};
+
 const main = (input) => {
   const numbers = input.map(line => {
-    const digitRegex = /\d/;
-    const firstDigit = line.match(digitRegex)[0];
-    const reversedLine = line.split('').reverse().join();
-    const lastDigit = reversedLine.match(digitRegex)[0];
-    return (Number(firstDigit) * 10) + Number(lastDigit);
+    const firstDigit = findDigit(line, sortByLowestIndexToHighest);
+    const lastDigit = findDigit(line, sortByHighestIndexToLowest);
+    return (Number(firstDigit) * 10) + Number(lastDigit)
   });
   return numbers.reduce((sum, num) => sum + num, 0);
 };
