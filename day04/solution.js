7486a7612bfc3c7e712f5ee946f5d063a1cd3d8a
Jeo D <jeo@dfor.dev>
2023-12-03 23:14:24 -0700

    Complete part 1 of day 4

--- day04/solution.js
+++ day04/solution.js
@@ -1,7 +1,16 @@
 import readInput from "../read-input.js";
 
-const part1 = (input) => {
+const calculateCardValue = (card) => {
+  const matches = card.replaceAll(/ +/g, ' ').match(/Card \d+: ((?:\d+ +)+)\| ((?:\d+ )+\d+)/);
+  const winningNumbers = matches[1].split(' ').filter(num => num !== '').map(num => Number(num));
+  const myNumbers = matches[2].split(' ').filter(num => num !== '').map(num => Number(num));
+  const myWinningNumsCount = myNumbers.filter(num => winningNumbers.includes(num)).length;
+  if (!myWinningNumsCount) return 0;
+  return 2 ** (myWinningNumsCount - 1);
+};
 
+const part1 = (input) => {
+  return input.reduce((sum, card) => sum + calculateCardValue(card), 0);
 };
 
 const part2 = (input) => {
---
+++ day04/test.js
@@ -0,0 +1,20 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import readInput from "../read-input.js";
+import { part1 } from "./solution.js";
+
+describe('day04 solution', () => {
+  describe('part 1', () => {
+    it('should return the correct value using the example input', () => {
+      const input = readInput('day04/example-input-1');
+      const result = part1(input);
+      assert.strictEqual(result, 13);
+    });
+
+    it('should return the correct value using the puzzle input', () => {
+      const input = readInput('day04/input');
+      const result = part1(input);
+      assert.strictEqual(result, 23941);
+    });
+  });
+});
