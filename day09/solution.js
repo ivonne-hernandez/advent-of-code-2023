91ad670f7467bdb1e1ecbf5907a1cd805bf7ebe8
Jeo D <jeo@dfor.dev>
2023-12-09 16:03:58 -0700

    Add day 9 skeleton

---
+++ day09/solution.js
@@ -0,0 +1,16 @@
+import readInput from '../read-input.js';
+
+const part1 = (input) => {
+
+};
+
+const part2 = (input) => {
+
+};
+
+if (process.env.NODE_ENV !== 'test') {
+  console.log('Part 1 solution:', part1(readInput()));
+  console.log('Part 2 solution:', part2(readInput()));
+}
+
+export { part1, part2 };
---
+++ day09/test.js
@@ -0,0 +1,14 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import readInput from "../read-input.js";
+import { part1 } from "./solution.js";
+
+describe('day09 solution', () => {
+  describe('part 1', () => {
+    it('should return the correct value using the example input', () => {
+      const input = readInput('day09/example-input-1');
+      const result = part1(input);
+      assert.strictEqual(result, 114);
+    });
+  });
+});
